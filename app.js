fce8036bf4af202bc69760d741ea48b4b3c3a085
Vikas <vikash.agarawal2012@gmail.com>
2023-02-13 11:03:19 +0530

    13-feb

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require("express")
 const app = express();
-
+const errorHandler=require("./middleware/error")
 app.use(express.json())
 //Route imports
 const categories  = require('./routes/home_routes')
 app.use("/api",categories)
+
+
+//Middleware for error
+app.use(errorHandler)
 module.exports = app;
--- controller/home_controller.js
+++ controller/home_controller.js
@@ -1,13 +1,17 @@
 const Category = require('../models/category');
-
+const catchAsyncErrors = require("../middleware/catchAsyncError")
 
 //Create Category--Admin Only
-exports.createCategory = async(req,res,next)=>{
+exports.createCategory = catchAsyncErrors(async(req,res,next)=>{
     const category  = await Category.create(req.body);
-    res.status(201).json({success:true,category}) 
-}
+    res.status(201).json({success:true,category}); 
+});
 //Get-Category
 exports.getCategories =async(req,res)=>{
     const category = await Category.find();
     res.status(200).json({success:true,category})
 }
+
+
+//update product 54:09  --admin only
+//    delete product--admin only
