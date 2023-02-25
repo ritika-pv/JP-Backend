8858178cfbbb1bb604a78dd0759c527f0cb69393
Vikas <vikash.agarawal2012@gmail.com>
2023-02-25 20:38:47 +0530

    little change in state

--- models/userModel.js
+++ models/userModel.js
@@ -1,129 +1,124 @@
-const mongoose=require("mongoose");
-const validator=require("validator");
-const bcrypt=require("bcrypt")
-const jwt=require("jsonwebtoken")
-const crypto=require("crypto")//built in module
+const mongoose = require("mongoose");
+const validator = require("validator");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const crypto = require("crypto"); //built in module
 
-const userSchema=new mongoose.Schema({
-    fname:{
-        type:String,
-        required:[true,"Please enter your first name"],
-        maxLength:[10,"First Name cannot exceed 30 characters"],
-        minLength:[2,"First Name should exceed 1 character "]
+const userSchema = new mongoose.Schema({
+  fname: {
+    type: String,
+    required: [true, "Please enter your first name"],
+    maxLength: [10, "First Name cannot exceed 30 characters"],
+    minLength: [2, "First Name should exceed 1 character "],
+  },
+  lname: {
+    type: String,
+    required: [true, "Please enter your last name"],
+    maxLength: [10, "Last Name cannot exceed 30 characters"],
+    minLength: [2, "Last Name should exceed 1 character "],
+  },
+  email: {
+    type: String,
+    required: [true, "Please enter your email"],
+    unique: true,
+    validate: [validator.isEmail, "Please enter a valid email"],
+  },
+  password: {
+    type: String,
+    required: [true, "Please enter your email"],
+    minLength: [8, "Password should exceed 8 characters "],
+    select: false,
+  },
+  avatar: {
+    public_id: {
+      type: String,
+      required: true,
     },
-    lname:{
-        type:String,
-        required:[true,"Please enter your last name"],
-        maxLength:[10,"Last Name cannot exceed 30 characters"],
-        minLength:[2,"Last Name should exceed 1 character "],
+    url: {
+      type: String,
+      required: true,
     },
-    email:{
-        type:String,
-        required:[true,"Please enter your email"],
-        unique:true,
-        validate:[validator.isEmail,"Please enter a valid email"],
-    },
-    password:{
-        type:String,
-        required:[true,"Please enter your email"],
-        minLength:[8,"Password should exceed 8 characters "],
-        select:false,
-    },
-    avatar:{
-        public_id:{
-            type:String,
-            required:true
-        },
-        url:{
-            type:String,
-            required:true
-        }
-    },
-    role:{
-        type:String,
-        default:"user",
-    },
+  },
+  role: {
+    type: String,
+    default: "user",
+  },
 
-    phone: {
-        type: String,
-        required: true,
-        validate: {
-          validator: function (v) {
-            return validator.isMobilePhone(v);
-          },
-          message: 'Invalid phone number'
-        }
+  phone: {
+    type: String,
+    required: true,
+    validate: {
+      validator: function (v) {
+        return validator.isMobilePhone(v);
       },
-      address: {
-        type: String,
-        required: true,
+      message: "Invalid phone number",
+    },
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  city: {
+    type: String,
+    required: true,
+  },
+  state: {
+    type: String,
+    required: true,
+    validate: {
+      validator: function (v) {
+        return validator.matches(v, /^[A-Za-z\s]+$/);
       },
-      city: {
-        type: String,
-        required: true,
+      message: "Invalid state name",
+    },
+  },
+  zip: {
+    type: String,
+    required: true,
+    validate: {
+      validator: function (v) {
+        return validator.isPostalCode(v, "any");
       },
-      state: {
-        type: String,
-        required: true,
-        validate: {
-          validator: function (v) {
-            return validator.isAlpha(v);
-          },
-          message: 'Invalid state name'
-        }
-      },
-      zip: {
-        type: String,
-        required: true,
-        validate: {
-          validator: function (v) {
-            return validator.isPostalCode(v, "any");
-          },
-          message: 'Invalid zip code'
-        }
-      },
-      resetPasswordToken:String,
-      resetPasswordExpire:Date,
-
-     
+      message: "Invalid zip code",
+    },
+  },
+  resetPasswordToken: String,
+  resetPasswordExpire: Date,
 });
-userSchema.pre("save",async function(next){
-    //when  the password is updated then only it  hash 
-    if(!this.isModified("password")){
-        next();
-    }
-    this.password=await bcrypt.hash(this.password,10);
-})
-
+userSchema.pre("save", async function (next) {
+  //when  the password is updated then only it  hash
+  if (!this.isModified("password")) {
+    next();
+  }
+  this.password = await bcrypt.hash(this.password, 10);
+});
 
 //JWT Token
-userSchema.methods.getJWTToken=function(){
-    return jwt.sign({id:this._id}, process.env.JWT_SECRET,{
-        expiresIn: process.env.JWT_EXPIRE,
-    })
-}
-
+userSchema.methods.getJWTToken = function () {
+  return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRE,
+  });
+};
 
 //compare password
-userSchema.methods.comparePassword=async function(enteredPassword){
- 
-    //compares text password to the hashed password`
-    return await bcrypt.compare(enteredPassword,this.password)
-}
-
+userSchema.methods.comparePassword = async function (enteredPassword) {
+  //compares text password to the hashed password`
+  return await bcrypt.compare(enteredPassword, this.password);
+};
 
 //Generating password reset token
 
-userSchema.methods.getResetPasswordToken=function(){
+userSchema.methods.getResetPasswordToken = function () {
   //Generating token
-  const resetToken=crypto.randomBytes(20).toString("hex");
- //hashing and adding to user schema
- this. resetPasswordToken=crypto.createHash("sha256").update(resetToken).digest("hex");
- this. resetPasswordExpire=Date.now()+15*60*1000;
- 
- return resetToken;
-}
+  const resetToken = crypto.randomBytes(20).toString("hex");
+  //hashing and adding to user schema
+  this.resetPasswordToken = crypto
+    .createHash("sha256")
+    .update(resetToken)
+    .digest("hex");
+  this.resetPasswordExpire = Date.now() + 15 * 60 * 1000;
 
+  return resetToken;
+};
 
-
-module.exports=mongoose.model("User",userSchema);
+module.exports = mongoose.model("User", userSchema);
